1ffd4fdd6f0d687c5c900217ea9af037113a5a75
Sean Bethel <sbethel@blueraster.com>
2017-11-26 21:09:10 -0500

    re-include .env until decided upon how to use it. Change compiled name to ElementaryModel.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'ElementaryModel.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: "var",
 	library: "g3n1us"
